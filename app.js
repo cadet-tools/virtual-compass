c2beeee79251149f4cffdc7b7f73f23297509999
cadet-tools <eglis.rjsv@gmail.com>
2025-12-20 23:31:21 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1570,33 +1570,21 @@ const lvmTopo10_wms = L.tileLayer.wms('https://lvmgeoserver.lvm.lv/geoserver/ows
   attribution: '© LVM, © LGIA'
 });
 
-// Aizstāj veco definīciju ar šo drošo versiju:
 const lvmTopoAuto_wms = L.tileLayer.wms('https://lvmgeoserver.lvm.lv/geoserver/ows?', {
   layers: 'public:LVM_Zemes_karte',
   format: 'image/png',
-  transparent: true,     // Svarīgi: true, lai fons būtu caurspīdīgs, kur nav datu
+  transparent: true,
   version: '1.1.1',
   tiled: true,
   maxZoom: 22,
   maxNativeZoom: 18,
-  
-  // 1. IEROBEŽOTĀJS: Šis ir kritiski svarīgi LVM kartei!
-  // Tas neļauj Leaflet prasīt datus ārpus Latvijas taisnstūra.
+  // IEROBEŽOTĀJS: Neļauj prasīt flīzes ārpus Latvijas (novērš 404/500 kļūdas)
   bounds: L.latLngBounds([55.60, 20.90], [58.10, 28.50]),
-
-  // 2. KĻŪDU SLĒPŠANA: Ja serveris tomēr nespēj iedot bildi, ieliekam tukšu pikseli.
-  errorTileUrl: 'data:image/gif;base64,R0lGODlhAQABAAAAACw=',
-  
+  errorTileUrl: 'data:image/gif;base64,R0lGODlhAQABAAAAACw=', // Slēpj "saplēsto" ikonu
   attribution: '© LVM, © LGIA'
 });
 
-// 3. KONSOLER TĪRĪTĀJS: Šis kods neļauj "tileerror" parādīties konsolē kā sarkanai kļūdai.
-// Tas "noķer" notikumu un apstādina to pirms tas nonāk konsolē.
-lvmTopoAuto_wms.on('tileerror', function(error, tile) {
-    // Pārliecināmies, ka kļūdainā flīze tiek aizstāta ar caurspīdīgu, lai lietotājs neredz "broken image"
-    error.tile.src = error.target.options.errorTileUrl;
-    // (Šeit apzināti nav console.error, lai netraucētu)
-});
+
 
 
 	  
